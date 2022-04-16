3ea21d16af8c6ea24e4b7388b2c784a4ecd3833c
Caeser F Ibrahim (he/him) <caeserlondon@gmail.com>
2022-04-16 21:13:03 +0700

    creating the players & targetting

--- game-logic.js
+++ game-logic.js
@@ -2,11 +2,30 @@ let playeText = document.getElementById("playeText");
 let restartBtn = document.getElementById("restartBtn");
 let boxes = Array.from(document.getElementsByClassName("box"));
 
-console.log(boxes);
+// console.log(boxes);
 
 const O_TEXT = "O";
 const X_TEXT = "X";
-let cuurentPlayer = X_TEXT;
+let currentPlayer = X_TEXT;
 let spaces = Array(9).fill(null);
 
-console.log(spaces);
+// console.log(spaces);
+
+const startGame = () => {
+	boxes.forEach((box) => box.addEventListener("click", boxClicked));
+};
+
+function boxClicked(e) {
+	// console.log(e.target);
+	const id = e.target.id;
+
+	if (!spaces[id]) {
+		spaces[id] = currentPlayer;
+
+		e.target.innerText = currentPlayer;
+
+		currentPlayer = currentPlayer == X_TEXT ? O_TEXT : X_TEXT;
+	}
+}
+
+startGame();
